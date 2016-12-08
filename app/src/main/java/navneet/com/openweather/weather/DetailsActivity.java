b993036c8fbfe1b35d91f78e8b8aaa335167fd4b
Navneet7k <krisnavneet.nk@gmail.com>
2016-12-09 00:06:17 +0530

    Added this week's forecast to recycler view

--- app/src/main/java/navneet/com/openweather/weather/DetailsActivity.java
+++ app/src/main/java/navneet/com/openweather/weather/DetailsActivity.java
@@ -4,12 +4,37 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 import navneet.com.openweather.R;
+import navneet.com.openweather.api.pojo.FiveWeathers;
+import navneet.com.openweather.api.pojo.Forecast;
+import navneet.com.openweather.data.WeatherObject;
+import navneet.com.openweather.util.Helper;
 
 
 public class DetailsActivity extends AppCompatActivity {
@@ -23,6 +48,10 @@ public class DetailsActivity extends AppCompatActivity {
     private TextView textView,text,dat,share,author,more;
     private ImageView imageView;
 
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private RequestQueue queue;
+
    // private WebView webView;
     private Button button;
     private String mCity,desc,date,image,auth;
@@ -33,17 +62,21 @@ public class DetailsActivity extends AppCompatActivity {
         textView=(TextView)findViewById(R.id.textView);
         author=(TextView)findViewById(R.id.textView8);
         text=(TextView)findViewById(R.id.textView2);
-        more=(TextView)findViewById(R.id.textView9);
-        share=(TextView)findViewById(R.id.textView7);
-        button=(Button)findViewById(R.id.button);
+      //  more=(TextView)findViewById(R.id.textView9);
+
+        queue = Volley.newRequestQueue(this);
         dat=(TextView)findViewById(R.id.textView4);
     //    webView=(WebView)findViewById(R.id.webView);
         imageView=(ImageView)findViewById(R.id.imageView1);
 
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(DetailsActivity.this, 4);
+
+        recyclerView = (RecyclerView)findViewById(R.id.weather_daily_list);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setHasFixedSize(true);
 
 
-
-
+/*
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,12 +95,11 @@ public class DetailsActivity extends AppCompatActivity {
                 what.setAction(Intent.ACTION_SEND);
                 what.putExtra(Intent.EXTRA_TEXT, "\nEvent:" + mCity + "\nJob Description:" +desc+ "\nExperience:" + date + "\nCTC:INR 12L - 1NR36L");
                 what.setType("text/plain");
- /*               what.putExtra(Intent.EXTRA_STREAM, uri);
-                what.setType("image/jpeg");
-                */
+ /
                 startActivity(Intent.createChooser(what, "Share with"));
             }
         });
+        */
        // read=getIntent().getStringExtra(EXTRA_URL);
         auth= getIntent().getStringExtra(EXTRA_PRECIP);
         mCity = getIntent().getStringExtra(EXTRA_CITY);
@@ -81,7 +113,91 @@ public class DetailsActivity extends AppCompatActivity {
         author.setText(auth);
 
 
+        fiveDaysApiJsonObjectCall(mCity);
+    }
 
+    private void fiveDaysApiJsonObjectCall(String city){
+        String apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q="+city+ "&APPID="+ Helper.API_KEY+"&units=metric";
+        final List<WeatherObject> daysOfTheWeek = new ArrayList<WeatherObject>();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, apiUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+              //  Log.d(TAG, "Response 5 days" + response);
+                GsonBuilder builder = new GsonBuilder();
+                Gson gson = builder.create();
+                Forecast forecast = gson.fromJson(response, Forecast.class);
+                if (null == forecast) {
+                    Toast.makeText(getApplicationContext(), "Nothing was returned", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Response Good", Toast.LENGTH_LONG).show();
+
+                    int[] everyday = new int[]{0,0,0,0,0,0,0};
+
+                    List<FiveWeathers> weatherInfo = forecast.getList();
+                    if(null != weatherInfo){
+                        for(int i = 0; i < weatherInfo.size(); i++){
+                            String time = weatherInfo.get(i).getDt_txt();
+                            String shortDay = convertTimeToDay(time);
+                            String temp = weatherInfo.get(i).getMain().getTemp();
+                            String tempMin = weatherInfo.get(i).getMain().getTemp_min();
+
+                            if(convertTimeToDay(time).equals("Mon") && everyday[0] < 1){
+                                daysOfTheWeek.add(new WeatherObject(shortDay, R.drawable.small_weather_icon, temp, tempMin));
+                                everyday[0] = 1;
+                            }
+                            if(convertTimeToDay(time).equals("Tue") && everyday[1] < 1){
+                                daysOfTheWeek.add(new WeatherObject(shortDay, R.drawable.small_weather_icon, temp, tempMin));
+                                everyday[1] = 1;
+                            }
+                            if(convertTimeToDay(time).equals("Wed") && everyday[2] < 1){
+                                daysOfTheWeek.add(new WeatherObject(shortDay, R.drawable.small_weather_icon, temp, tempMin));
+                                everyday[2] = 1;
+                            }
+                            if(convertTimeToDay(time).equals("Thu") && everyday[3] < 1){
+                                daysOfTheWeek.add(new WeatherObject(shortDay, R.drawable.small_weather_icon, temp, tempMin));
+                                everyday[3] = 1;
+                            }
+                            if(convertTimeToDay(time).equals("Fri") && everyday[4] < 1){
+                                daysOfTheWeek.add(new WeatherObject(shortDay, R.drawable.small_weather_icon, temp, tempMin));
+                                everyday[4] = 1;
+                            }
+                            if(convertTimeToDay(time).equals("Sat") && everyday[5] < 1){
+                                daysOfTheWeek.add(new WeatherObject(shortDay, R.drawable.small_weather_icon, temp, tempMin));
+                                everyday[5] = 1;
+                            }
+                            if(convertTimeToDay(time).equals("Sun") && everyday[6] < 1){
+                                daysOfTheWeek.add(new WeatherObject(shortDay, R.drawable.small_weather_icon, temp, tempMin));
+                                everyday[6] = 1;
+                            }
+                            recyclerViewAdapter = new RecyclerViewAdapter(DetailsActivity.this, daysOfTheWeek);
+                            recyclerView.setAdapter(recyclerViewAdapter);
+                        }
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+              //  Log.d(TAG, "Error " + error.getMessage());
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    private String convertTimeToDay(String time){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:SSSS", Locale.getDefault());
+        String days = "";
+        try {
+            Date date = format.parse(time);
+            System.out.println("Our time " + date);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            days = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault());
+            System.out.println("Our time " + days);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return days;
     }
 
 

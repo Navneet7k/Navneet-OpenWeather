9e026353ce2509f7a8bce09d2a1bdff010db533e
Navneet7k <krisnavneet.nk@gmail.com>
2016-12-09 09:44:14 +0530

    Added search to check if clicking search field yields search dialog

--- app/src/androidTest/java/navneet/com/openweather/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/navneet/com/openweather/ExampleInstrumentedTest.java
@@ -2,11 +2,20 @@ package navneet.com.openweather;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import navneet.com.openweather.weather.WeatherActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -18,9 +27,21 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
+
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("navneet.com.openweather", appContext.getPackageName());
     }
+    @Rule
+    public ActivityTestRule<WeatherActivity> mNotesActivityTestRule =
+            new ActivityTestRule<>(WeatherActivity.class);
+
+    @Test
+    public void clickAddNoteButton_opensAddNoteUi() throws Exception {
+
+        onView(withId(R.id.edittext)).perform(click());
+
+        onView(withId(R.id.et_city)).check(matches(isDisplayed()));
+    }
+
 }
